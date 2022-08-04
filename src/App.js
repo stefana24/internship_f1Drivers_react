063b66b1571fadd555ecf606296db09c83800f9d
stefana <stefi_campean@yahoo.com>
2022-08-04 10:31:11 +0300

    sorting

--- src/App.js
+++ src/App.js
@@ -4,23 +4,30 @@ import { Header } from "./Components/Header";
 import { Card } from "./Components/Card";
 
 import { mockData } from "./utils/mockData.js";
+import { useState } from "react";
 
 function App() {
-  const mockDataSorted = [...mockData].sort((a, b) =>
-    a.points > b.points
-      ? -1
-      : a.points === b.points
-      ? a.firstName > b.firstName
-        ? 1
-        : -1
-      : 1
-  );
+  const [sortDrivers, setSortedDrivers] = useState(mockData);
+
+  function handleSort() {
+    const mockDataSorted = [...sortDrivers].sort((a, b) => {
+      return a.points > b.points
+        ? -1
+        : a.points === b.points
+        ? a.firstName > b.firstName
+          ? 1
+          : -1
+        : 1;
+    });
+    setSortedDrivers(mockDataSorted);
+  }
+
   return (
     <div className="App">
       <Header />
 
-      <div className="cards">
-        {mockDataSorted.map((driver, index) => (
+      <div className="cards" onLoad={handleSort}>
+        {sortDrivers.map((driver, index) => (
           <Card key={driver.number} driver={driver} index={index} />
         ))}
       </div>
